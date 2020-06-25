e926a922b710089daa25428dbfc5c4ce4bcb6d7b
rlagustn0724 <rlagustn0724@naver.com>
2020-06-25 12:47:21 +0900

    랭킹

--- sms.py
+++ sms.py
@@ -14,6 +14,26 @@ class Sms(TestCase):
             std = line[i].split(",") 
             sum.append(int(std[2])+int(std[3])+int(std[4]))
             avg.append(round(sum[i]/3,3))
- 
+
+        ranking = list()
+        self.assertIsNotNone(ranking)      
+
+        for i in range(len(line)):
+            std = line[i].split(",") 
+            sum.append(int(std[2])+int(std[3])+int(std[4]))
+            avg.append(round(sum[i]/3,3))
+
+        ranking = list()
+        self.assertIsNotNone(ranking)
+        
+        for content in sum:
+            r = 1
+            for content2 in sum:
+                if content < content2:
+                    r=r+1
+            ranking.append(r)
+
+        fp.close()
+          
 if(__name__=="__main__"):
     unittest.main()
